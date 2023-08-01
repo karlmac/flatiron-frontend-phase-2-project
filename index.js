cf179843dae230f265b76c64809fcb23480a9970
Karlmac Yerima <yerimakarlmac@gmail.com>
2023-08-01 00:56:31 -0500

    Added eventlisteners for HTML lements

---
+++ index.js
@@ -0,0 +1,24 @@
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+    const lastPunchMsg = document.getElementById("lastPunchMsg");
+    const btnSubmit = document.getElementById("btnSubmit");
+    const btnShowHistory = document.getElementById("btnShowHistory");
+    const divHistory = document.getElementById("divHistory");
+    
+
+    //Get Last Punch info
+    lastPunchMsg.textContent = `Last clocked ${5} at ${5}`;
+
+    //Submit Punch
+    btnSubmit.addEventListener("click", () => {
+        // 
+        
+      })
+      
+    //Show Punch history
+    btnShowHistory.addEventListener("click", () => {
+        // 
+        
+      })
+});
