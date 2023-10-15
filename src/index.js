949ccb232a5cee6b7f462d289bfcf1da6844c003
Karlmac Yerima <yerimakarlmac@gmail.com>
2023-10-15 06:11:25 -0500

    Add icon and public folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,103 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
     <h1>Time Card</h1>,
   document.getElementById('main')
 );
+
+
+class TimeCardApp extends Component {
+  constructor(props) {
+    super(props);
+
+    // Initial state
+    this.state = {
+      day: '',            // To store the selected day
+      timeIn: '',         // To store the clock-in time
+      timeOut: '',        // To store the clock-out time
+      punchHistory: [],   // To store the punch history (an array of objects)
+      showPunchHistory: false, // To control the display of punch history
+      actionMessage: '',  // To display action messages
+    };
+  }
+
+  // Event handler for day input change
+  handleDayChange = (event) => {
+    this.setState({ day: event.target.value });
+  };
+
+  // Event handler for time in input change
+  handleTimeInChange = (event) => {
+    this.setState({ timeIn: event.target.value });
+  };
+
+  // Event handler for time out input change
+  handleTimeOutChange = (event) => {
+    this.setState({ timeOut: event.target.value });
+  };
+
+  // Event handler for submitting a punch
+  handleSubmit = () => {
+    // Your existing submitPunch logic here
+    // Make sure to update state accordingly after submission
+  };
+
+  // Event handler for showing/hiding punch history
+  togglePunchHistory = () => {
+    // Your existing showPunchHistory logic here
+    // Update this.state.showPunchHistory accordingly
+  };
+
+  // Fetch punch history and update state
+  fetchPunchHistory = async () => {
+    // Your fetch logic here to retrieve punch history
+    // Update this.state.punchHistory with the fetched data
+  };
+
+  // Other methods as needed
+
+  render() {
+    const { day, timeIn, timeOut, punchHistory, showPunchHistory, actionMessage } = this.state;
+
+    return (
+      <div>
+        <h1>Time Card</h1>
+        <label htmlFor="dayPunch">Day:</label>
+        <input
+          type="date"
+          id="dayPunch"
+          value={day}
+          onChange={this.handleDayChange} // Event handler for day input
+        />
+
+        <label htmlFor="timePunchIn">Enter Clock-In Time:</label>
+        <input
+          type="time"
+          id="timePunchIn"
+          value={timeIn}
+          onChange={this.handleTimeInChange} // Event handler for time in input
+        />
+
+        <label htmlFor="timePunchOut">Enter Clock-Out Time:</label>
+        <input
+          type="time"
+          id="timePunchOut"
+          value={timeOut}
+          onChange={this.handleTimeOutChange} // Event handler for time out input
+        />
+
+        <button onClick={this.handleSubmit}>Submit</button> {/* Event handler for submit button */}
+        
+        <button onClick={this.togglePunchHistory}>
+          {showPunchHistory ? "Hide History" : "Show History"}
+        </button> {/* Event handler for show/hide history button */}
+        
+        {/* Your HTML/JSX structure for punch history display */}
+      </div>
+    );
+  }
+}
+
+export default TimeCardApp;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const cpath = import('path');
+const HtmlWebpackPlugin = import('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
+  /* entry: cpath.join(__dirname, "src", "index.js"),
   output: {
-    path:path.resolve(__dirname, "dist"),
+    path:cpath.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
@@ -22,7 +22,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
+      template: cpath.join(__dirname, "src", "index.html"),
     }),
-  ],
+  ],*/
 }
